054030237605eee5306b51a4427eeb93bdc7d1c6
rahernandez <robertoh@outlook.com>
2014-05-29 07:38:04 -0500

    Update ex33.py

--- ex33.py
+++ ex33.py
@@ -14,17 +14,19 @@ print "the numbers: "
 for num in numbers:
 	print num
 	
-#Drill
-def countNum (b):
+#Drills, 3-4
+def countNum (b, inc):
 	i = 0
 	numbers = []
+	
 	while i < b:
 		print "At the top i is % d" % i
 		numbers.append(i)
 	
-		i = i + 1
+		i = i + inc
 		print "Numbers now: ", numbers
 		print "At the bottom i is %d" % i
 	
 	return b
-countNum(90)
+countNum(90, 9)
+#
