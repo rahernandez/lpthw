ccb19b5228ab8daef8b48c1569a7e5b70cbccafa
rahernandez <robertoh@outlook.com>
2014-06-04 10:30:31 -0500

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,26 @@
+# This line prints the sentence "I will now.."
+print "I will now count my chickens:"
+
+#this line divides 30 by 6 and then adds 25 to it
+print "Hens", 25 + 30/ 6
+#this line  multiplies 25 x 3 which is 75, then takes module of 75 % 4 which is 3, then finally we do subtraction of 100-3 which is 97
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3  + 2 + 1 -5 + 4 % 2 - 1 / 4 + 6
+
+print "is it true that  3 + 2  < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "what is 3 + 2", 3 +2
+print "what is 5 - 7?", 5-7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "is greater?", 5 > -2
+print "Is it greater or equal?", 5 >=2
+print "Is it less or equal?", 5<=-2
